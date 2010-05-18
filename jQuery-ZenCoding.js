7b5ba2b7e3b9381e00c977d62d6020a7989d8ebe
zodoz <zdz.ruai@gmail.com>
2010-05-18 03:16:28 -0500

    started recursive function...  still needs a lot of work.

--- jQuery-ZenCoding.js
+++ jQuery-ZenCoding.js
@@ -1,5 +1,51 @@
 (function($) {
- 	$.fn.zen = function(ZenCode) {
-		
+ 	$.zc = function(ZenCode) {
+		return createHTMLBlock(ZenCode);
 	};
+
+	function createHTMLBlock(ZenCode) {
+		var regBlock = /((\w+)[#.]?)+/gi,
+			regTag = /(\w+)/i,
+			regId = /#(\w+)/i,
+			regClass = /\.(\w+)/i;
+		var blocks = ZenCode.match(regBlock);
+		console.log(blocks);
+		if(blocks.length < 1)	//no more blocks to match
+			return;
+		var block = blocks[0];	//actual block to create
+		if(regId.test(block))
+			var blockId = regId.exec(block)[1];
+		var blockClasses = getClasses(block);
+		var blockTag = regTag.exec(block)[1];
+		var tagString = '<'+blockTag;
+		if(blockId !== undefined)
+			tagString += ' id="'+blockId+'"';
+		if(blockClasses !== undefined)
+			tagString += ' class="'+blockClasses+'"';
+		tagString += '></'+blockTag+'>';
+		console.log(tagString);
+		var el = $(tagString);
+		ZenCode = ZenCode.substr(blocks[0].length);
+		if(ZenCode.length > 0) {
+			console.log(ZenCode);
+			if(ZenCode[0] == '+')
+				el.after(createHTMLBlock(ZenCode.substr(1)));
+			else if(ZenCode[0] == '>')
+				el.html(createHTMLBlock(ZenCode.substr(1)));
+		}
+		return el;
+	}
+
+	function getClasses(ZenBlock) {
+		var regClasses = /(\.\w+)/gi,
+			regClass = /\.(\w+)/i;
+		if(ZenBlock.search(regClasses) == -1)
+			return undefined;
+		var classes = ZenBlock.match(regClasses);
+		var clsString = '';
+		for(var i=0;i<classes.length;i++) {
+			clsString += ' '+regClass.exec(classes[i])[1];
+		}
+		return clsString.trim();
+	}
  })(jQuery);
--- test.js
+++ test.js
@@ -1,3 +1,44 @@
 $().ready(function() {
-	
+	$('#area').val('');
+	var count=0;
+	function test(ZenCode) {
+		var area = $('#area');
+		area.append(
+			$('<div id="test('+ZenCode+')"></div>').append($.zc(ZenCode))
+		);
+	}
+	/*
+	- tag
+	- tag#id
+	- tag.class
+	- tag.class.class2
+	- tag#id.class???
+	- tag.class#id???
+	- tag>tag2
+	- tag#id>tag2
+	- tag.class>tag2
+	- tag>tag2#id
+	- tag>tag2.class
+	- tag#id>tag2#id
+	- tag#id>tag2.class
+	- tag.class>tag2#id
+	- tag.class+tag2.class
+	- tag+tag2
+	- tag#id+tag2
+	- tag.class+tag2
+	- tag+tag2#id
+	- tag+tag2.class
+	- tag#id+tag2#id
+	- tag#id+tag2.class
+	- tag.class+tag2#id
+	- tag.class+tag2.class
+	*/
+
+	test('tag');
+	test('tag#id');
+	test('tag.class');
+	test('tag.class.class2');
+	test('tag#id.class.class2');
+	test('tag>tag2');
+	test('tag+tag2');
 });
